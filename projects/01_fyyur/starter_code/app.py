f199bc0af5d103ef03c5a87e5bd0c0a6d989180e
Adrien <adrienunger@gmx.de>
2020-04-05 19:49:22 +0200

    flask migrations: added missing fields for venue and artist as well as constraints

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -33,31 +33,52 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(12), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.Text)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # genres
+    # website
+    # seeking_talent
+    # seeking_description
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(12), nullable=False)
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.Text)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # website
+    # seeking_venue
+    # seeking_description
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+# Show
+  # venue_id
+    # venue_ name
+  # artist_id
+    # artist_name
+    # artist_image_link
+  # start_time
 
 #----------------------------------------------------------------------------#
 # Filters.
